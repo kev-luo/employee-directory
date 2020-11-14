1591f4be4e100d598a48f0003af91401d92d5c76
Kevin Luo <kvn.luo@gmail.com>
2020-11-14 11:27:14 -0500

    menubar style

--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -1,13 +1,32 @@
 import React from 'react'
 import './MenuBar.css';
-import { AppBar, Toolbar, Typography } from '@material-ui/core';
+import { AppBar, Toolbar, Typography, Paper } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function MenuBar() {
+const useStyles = makeStyles({
+  root: {
+    backgroundColor: '#fafafa',
+    padding: 0,
+    margin: 0,
+    textAlign: 'center',
+  },
+  appbar: {
+    alignItems: 'flex-start',
+    background: 'linear-gradient(to right, #0040ff, #809fff)'
+  }
+})
+
+function MenuBar() {
+  const classes = useStyles();
   return (
-    <AppBar color="primary" position="relative">
-      <Toolbar>
-        <Typography color="inherit" variant='h6'>Employee Search</Typography>
-      </Toolbar>
-    </AppBar>
+    <Paper elevation={0} className={classes.root}>
+      <AppBar position="relative" className={classes.appbar}>
+        <Toolbar>
+          <Typography color="inherit" variant='h6'>Employee Search</Typography>
+        </Toolbar>
+      </AppBar>
+    </Paper>
   )
 }
+
+export default MenuBar;
