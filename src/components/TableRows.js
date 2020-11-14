b13fc8b77f51714e8bbee32ea458b96bbc282abe
Kevin Luo <kvn.luo@gmail.com>
2020-11-14 13:11:49 -0500

    sort all columns except images

--- src/components/TableRows.js
+++ src/components/TableRows.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect } from 'react'
 import _ from 'lodash';
 import { TableContainer, Table, TableHead, TableRow, TableCell, TableBody, Paper } from '@material-ui/core';
 import { getUsers } from '../utils/API';
@@ -19,7 +19,6 @@ export default function TableRows() {
         email: user.email,
         age: user.dob.age,
         city: user.location.city,
-        state: user.location.state,
         country: user.location.country,
         image: user.picture.thumbnail,
       }
@@ -33,19 +32,20 @@ export default function TableRows() {
     searchApi();
     setCategories([
       {name: 'Image', isSorted: false}, 
-      {name: 'Name', isSorted: false}, 
+      {name: 'First Name', isSorted: false},
+      {name: 'Last Name', isSorted: false}, 
       {name: 'Age', isSorted: false}, 
-      {name: 'Location', isSorted: false}, 
+      {name: 'City', isSorted: false},
+      {name: 'Country', isSorted: false}, 
       {name: 'Email', isSorted: false},
     ])
   },[])
 
   const sortColumn = (column, isSorted) => {
-    console.log(column);
     if (isSorted) {
-      setPeople(_.orderBy(people, column.toLowerCase(), ['asc']));
+      setPeople(_.orderBy(people, column.split(' ').join('').toLowerCase(), ['desc']));
     } else {
-      setPeople(_.orderBy(people, column.toLowerCase(), ['desc']));
+      setPeople(_.orderBy(people, column.split(' ').join('').toLowerCase(), ['asc']));
     }
     setCategories(categories.map(category => {
       if (category.name === column) {
@@ -62,7 +62,9 @@ export default function TableRows() {
         <TableHead>
           <TableRow>
             {isLoading || categories.map(({name, isSorted}) => {
-              return (
+              return name === 'Image' ? (
+                <TableCell key={name}>{name}</TableCell>
+              ) : (
                 <TableCell key={name} onClick={() => sortColumn(name, isSorted)}>{name}</TableCell>
               )
             })}
@@ -73,9 +75,11 @@ export default function TableRows() {
             return (
               <TableRow key={person.id}>
                 <TableCell><img src={person.image} alt={person.firstname} /></TableCell>
-                <TableCell>{person.firstname} {person.lastname}</TableCell>
+                <TableCell>{person.firstname}</TableCell>
+                <TableCell>{person.lastname}</TableCell>
                 <TableCell>{person.age}</TableCell>
-                <TableCell>{person.city}, {person.state} - {person.country}</TableCell>
+                <TableCell>{person.city}</TableCell>
+                <TableCell>{person.country}</TableCell>
                 <TableCell>{person.email}</TableCell>
               </TableRow>
             )
